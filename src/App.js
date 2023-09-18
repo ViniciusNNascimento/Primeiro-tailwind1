4f7703bf2a0d485068934955ff6db39b8a51b630
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2023-09-18 15:57:06 -0300

    carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { NavbarDefault } from "./components/Navbar";
+import { CarouselDefault, CarouselTransition } from "./components/Hero";
 
 function App() {
   return (
     <div>
-      <NavbarDefault />
+      <NavbarDefault/>
+      <CarouselDefault/>
+
     </div>
   );
 }
